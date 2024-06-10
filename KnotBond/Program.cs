813b22eed81e1f40e1f8d7b90571f99ecf32a9fb
mahmoud samir <mahmoudsamirms01@gmail.com>
2024-06-10 22:48:26 +0300

    Create Angular client with CA

--- KnotBond/Program.cs
+++ KnotBond/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -23,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
